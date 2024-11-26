6eb5b35bc3727a124d64ce3e67170cf9ce8e0fae
duydonv <phamduydong1000@gmail.com>
2024-11-26 13:33:11 +0700

    implementation of the class Store

--- Lab02/AimsProject/DigitalVideoDisc.java
+++ Lab02/AimsProject/DigitalVideoDisc.java
@@ -3,52 +3,92 @@ package AimsProject;
 public class DigitalVideoDisc {
     private String title;
     private String category;
-    private String director;        //Pham Duy Dong - 5608
+    private String director;
     private int length;
     private float cost;
-    
-    public String getTitle() {
-        return title;
-    }
-    public String getCategory() {
-        return category;
-    }
-    public String getDirector() {
-        return director;                //Pham Duy Dong - 5608
-    }
-    public int getLength() {
-        return length;
-    }
-    public float getCost() {
-        return cost;
-    }
 
-    // Constructor 1: Create DVD by title
+    // Thuộc tính lớp (static): Theo dõi số lượng DVD đã được tạo
+    private static int nbDigitalVideoDiscs = 0;
+    // Thuộc tính đối tượng: ID duy nhất của mỗi DVD
+    private int id;
+
+    // Constructor 1: Tạo DVD chỉ với title
     public DigitalVideoDisc(String title) {
         this.title = title;
+        // Tăng số lượng DVD đã được tạo
+        nbDigitalVideoDiscs++;
+        // Gán ID cho DVD
+        this.id = nbDigitalVideoDiscs;
     }
 
-    // Constructor 2: Create DVD by category, title, and cost
+    // Constructor 2: Tạo DVD với category, title, cost
     public DigitalVideoDisc(String category, String title, float cost) {
         this.category = category;
-        this.title = title;																		//Pham Duy Dong - 5608
+        this.title = title;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
-    // Constructor 3: Create DVD by director, category, title, and cost
+    // Constructor 3: Tạo DVD với director, category, title, cost
     public DigitalVideoDisc(String director, String category, String title, float cost) {
         this.director = director;
         this.category = category;
-        this.title = title;																		//Pham Duy Dong - 5608
+        this.title = title;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
 
-    // Constructor 4: Create DVD by all attributes: title, category, director, length, cost
+    // Constructor 4: Tạo DVD với tất cả thuộc tính
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         this.title = title;
         this.category = category;
         this.director = director;
         this.length = length;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
+
+    // Getter cho ID
+    public int getId() {
+        return id;
+    }
+
+    // Getter cho số lượng DVD đã tạo
+    public static int getNbDigitalVideoDiscs() {
+        return nbDigitalVideoDiscs;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String toString() {
+	    return "DVD - " + title + " - " + category + " - " + director + " - " + length + ": " + cost + " $";
+	}
+	public boolean isMatch(String title) {
+	    return this.title.equalsIgnoreCase(title);
+	}
+
 }
