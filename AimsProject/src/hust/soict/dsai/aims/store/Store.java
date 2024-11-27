e5ad4fd02d7c8fe63e412c59512dd44ba622f052
duydonv <phamduydong1000@gmail.com>
2024-11-27 16:39:26 +0700

    khac phuc het cac canh bao

--- AimsProject/src/hust/soict/dsai/aims/store/Store.java
+++ AimsProject/src/hust/soict/dsai/aims/store/Store.java
@@ -1,6 +1,8 @@
 package hust.soict.dsai.aims.store;
 
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
 import java.util.ArrayList;
+
 
 public class Store {
     private ArrayList<DigitalVideoDisc> itemsInStore; 
